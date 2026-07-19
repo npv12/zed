63e7b44ff40b0a72f94f2b2eeaae1ec838b2d38d
Pranav <pranav10121@gmail.com>
2026-07-19 20:58:01 +0530

    git_graph: Add show_remote_branches to GraphLogOptions with ToggleShowRemoteBranches
    - Add show_remote_branches field to GraphLogOptions
    - Make --remotes conditional on show_remote_branches in get_args()
    - Add ToggleShowRemoteBranches action and dropdown checkbox
    
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- crates/git/src/repository.rs
+++ crates/git/src/repository.rs
@@ -816,6 +816,7 @@ pub enum LogSource {
 pub struct GraphLogOptions {
     pub show_stashes: bool,
     pub show_tags: bool,
+    pub show_remote_branches: bool,
 }
 
 impl Default for GraphLogOptions {
@@ -823,6 +824,7 @@ impl Default for GraphLogOptions {
         Self {
             show_stashes: true,
             show_tags: true,
+            show_remote_branches: true,
         }
     }
 }
@@ -834,8 +836,10 @@ impl GraphLogOptions {
                 let mut args = vec![
                     Cow::Borrowed("--ignore-missing"),
                     Cow::Borrowed("--branches"),
-                    Cow::Borrowed("--remotes"),
                 ];
+                if self.show_remote_branches {
+                    args.push(Cow::Borrowed("--remotes"));
+                }
                 if self.show_tags {
                     args.push(Cow::Borrowed("--tags"));
                 }
